502f85fe8e0b50ccadf859662f791ef36859d7ab
Fabian Global Services <fabiankarell82@gmail.com>
2025-12-02 11:59:11 -0500

    Linked to OMdb movie API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { movies } from './data';
-import Movies from './pages/Movies';
-import MovieInfo from './pages/MovieInfo';
-import Home from './pages/Home'; 
-import Nav from './components/Nav';
-import Footer from './components/Footer';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import MovieInfo from "./pages/MovieInfo";
+import { useEffect, useState } from "react";
+import Nav from "../src/components/Nav";
+import Footer from "../src/components/Footer";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch(
+        `https://www.omdbapi.com/?apikey=747abfb4&s=fast`
+      );
+      const data = await response.json();
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
+  }, []);
+
   return (
-    <Router>
+    <Router basename="/react-project-fast-and-the-furious">
       <div>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home movies={movies} />} />        
-        <Route path="/movies" element={<Movies movies={movies} />} />
-        <Route path="/movie/:id" element={<MovieInfo movies={movies} />} />
-      </Routes>
+     <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movie/:id" element={<MovieInfo />} />
+    </Routes>
       <Footer />
-      </div>
+    </div>
     </Router>
   );
 }
